58f5ad2d616aa85f08f75ab640f7729a80952b32
IuriSampaio <iurisampaio18@gmail.com>
2020-07-20 22:40:47 -0300

    update entrada dados

--- EntradaDados/UsandoDados.sh
+++ EntradaDados/UsandoDados.sh
@@ -2,3 +2,8 @@ echo "digite seu nome:"
 read nome
 # para usar uma variavel digitada pelo usuario basta colocar '$' antes do nome da variavel
 echo "eae $nome!"
+
+###### OUTRA FORMA DE USAR #######
+read -p "Digite sua idade: "
+# chamando variavel que foi lida e não deu nome usa-se $REPLY
+echo "sua idade é $REPLY"
